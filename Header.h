af7c8f00da103fc7c82aa6f8937e4b249dfefd71
Hole-113 <122269835+Hole-113@users.noreply.github.com>
2025-01-20 17:51:17 +0530

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,3 +1,23 @@
 #include<stdio.h>
 #include<tc/tc_startup.h>
 #include<tcinit/tcinit.h>
+#include<tc/emh.h>
+#include<tccore/item.h>
+#include<sa/user.h>
+#include<tc/folder.h>
+#include<tccore/aom.h>
+
+int Report_Error(int iFail)
+{
+	char* cError = NULL;
+	if (iFail != ITK_ok)
+	{
+		EMH_ask_error_text(iFail, &cError);
+		printf("\n\n Error is : %s", cError);
+		MEM_free(cError);
+		exit(0);
+	}
+	return 0;
+}
+
+
